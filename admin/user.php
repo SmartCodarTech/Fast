c9da3efb4aa51830efffaef7d12597460ca5353a
Wisdom Kukah <eleiknana45@gmail.com>
2019-03-15 12:25:28 +0000

    Add existing file

---
+++ admin/user.php
@@ -0,0 +1,62 @@
+<div class="modal fade" id="addnew">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Add New Admin</b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="add_user.php" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="Username" class="col-sm-3 control-label">Username</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="firstname" class="col-sm-3 control-label">Firstname</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="firstname" name="firstname" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="lastname" class="col-sm-3 control-label">Lastname</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="lastname" name="lastname" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-sm-3 control-label">Password</label>
+
+                    <div class="col-sm-9">
+                      <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-sm-3 control-label">Confrim Password</label>
+
+                    <div class="col-sm-9">
+                      <input type="password" class="form-control" id="confirm_password" name="confrim_password" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="photo" class="col-sm-3 control-label">Photo</label>
+
+                    <div class="col-sm-9">
+                      <input type="file" id="photo" name="photo">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+              <button type="submit" class="btn btn-primary btn-flat" name="submit"><i class="fa fa-save"></i>Add</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div>
--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
     <div class="container">
       <div class="pull-right hidden-xs">
-        <b>Voting System</b>
+        <b>WASS Voting System</b>
       </div>
-      <strong> &copy; 2018 Brought To You By<a href="https://code-projects.org/"> Code-Projects</a></strong>
+      <strong> &copy; 2019 Brought To You By<a href="https://code-projects.org/"> CodeBrainGh</a></strong>
     </div>
     <!-- /.container -->
 </footer>
